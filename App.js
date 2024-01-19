503dccbffd7fb2fc06a1c23b2c5fd2b83345281b
Jacob Jonsson <jacobjjonsson@gmail.com>
2024-01-18 19:57:10 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 export default function App() {
+  const [permission, askForPermission] =
+    ImagePicker.useMediaLibraryPermissions();
+
+  async function handlePress() {
+    await askForPermission();
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+    });
+
+    if (result.canceled || !result.assets[0]) {
+      return;
+    }
+
+    try {
+      await FileSystem.uploadAsync(
+        "https://httpbin.org/post",
+        result.assets[0].uri,
+        {
+          fieldName: "file",
+          mimeType: "image/jpeg",
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Open picker</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
